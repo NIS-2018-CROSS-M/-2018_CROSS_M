bf1e4bc195eac331cb77d9632bccdc71fb26483b
Lorenzo T <ltosi@edu.hse.ru>
2019-01-14 15:53:41 +0300

    n-extractor minor fixes

--- DATA/n-extractor.py
+++ DATA/n-extractor.py
@@ -20,13 +20,13 @@ def get_interesting_analyses_from_lexeme_analysis_line(line):
     return lexeme, interesting_analyses
 
 
-def check_for_indeclensible(lexeme, analysis, language):
+def check_for_undeclinables(lexeme, analysis, language):
     # how many cases in noun readings for every slavic language #
     declensions = {"rus": 12, "ces": 14, "pol": 12, "ukr": 14, "bel": 12, "bul": 6,
                    "mkd": 4, "slv": 18, "sr": 14, "hr": 14, "sil": 12}
     # clean lexeme from numbers and symbols to compare it with analysis readings #
     lexeme = re.sub("[0-9]+ \\^", "", lexeme)
-    # if all lexemes are the same in analyses, and they are included for all cases, put word in indeclensible readings #
+    # if all lexemes are the same in analyses, and they are included for all cases, put word in undeclinable readings #
     if all(lexeme in analyze for analyze in analysis):
         if len(analysis) == declensions[language]:
             return True
@@ -41,17 +41,17 @@ if __name__ == "__main__":
     for filean in os.listdir(path="./files/"):
         if filean.endswith(".txt"):
             good_readings = []
-            indeclensible_readings = []
+            undeclinable_readings = []
             with open("./files/" + filean, "r", encoding="utf-8") as f:
                 for line in f.readlines():
                     lex, ans = get_interesting_analyses_from_lexeme_analysis_line(line)
                     if len(ans) != 0:
-                        if check_for_indeclensible(lex, ans, re.sub("10000.txt", "", filean)) is False:
+                        if check_for_undeclinables(lex, ans, re.sub("10000.txt", "", filean)) is False:
                             good_readings.append(lex + "/" + "/".join(ans))
                         else:
-                            indeclensible_readings.append(lex + "/" + "/".join(ans))
+                            undeclinable_readings.append(lex + "/" + "/".join(ans))
             # be sure to create a folder named "cleaned" inside your folder with frequency files #
             with open("./files/cleaned/" + filean, "w+", encoding="utf-8") as w:
                 w.write("".join(good_readings))
             with open("./files/cleaned/" + "indeclensible" + filean, "w+", encoding="utf-8") as w:
-                w.write("".join(indeclensible_readings))
+                w.write("".join(undeclinable_readings))
