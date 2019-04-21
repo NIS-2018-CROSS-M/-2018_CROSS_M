a299b703d5a1d86efe89aa8b389bb23a719b6331
Lorenzo T <ltosi@edu.hse.ru>
2019-04-21 22:54:20 +0300

    fixed all tags from new_tags, time to test it

--- DATA/table-creator.py
+++ DATA/table-creator.py
@@ -1,4 +1,30 @@
 udtags = {
+    # unidirectional tags (not present in UD or used differently)
+    '<iv>': 'Valency=1',
+    '<tv>': 'Valency=2',
+    '<ref>': 'Valency=Refl',  # which tagset to follow for this?
+    '<emph>': 'VerbType=Emph',  # check consistency with other tagsets
+    '<fac>': 'VerbType=Fact',  # don't add this one наверн, эта разметка на типы глагола есть только в Апертиуме
+    '<itg>': 'Type=Inter',  # Possible for verbs as well as for adj and adv, check how to tag properly
+    '<cmp>': 'Degree=Com',
+    '<prx>': 'Definite=Prx',  # Proximal defined article, Macedonian (no UD model)
+    '<dst>': 'Definite=Dst',  # Distal defined article, Macedonian (no UD model)
+    '<adj><po>': 'ADV',  # Czech, chorvatsky = по-хорватски, it is an "adverbiated adjective"
+    '+htjeti': '',
+    # Croatian, всегда ходят такой цепочкой: <inf>+htjeti<vbmod>. Excluding modal as it concurs to form a regular future form. встречается как кластер <inf>+htjeti<vbmod><clt><futI>. Здесь конструкция хотеть+глагол, где клитик отвечает за окончание глагола “хотеть” (htjeti)
+    '<vbmod>': '',
+    # Excluding modal as it is a closed class, only in readings like htjeti or moguci (serbian), where it is an adverbial derived form
+    '<clt>': '',
+    # clitics, same reason as per '+htjeti', could be used to mark the "reduced" form of FutI in serbo-croatian if we want to mark it (brace vs. brat ce (more common))
+    '+pos<adj><mi>': ('Poss=Yes', 'PossGender=Masc'),
+    # same reason as htjeti, used to build possessive constructions for name (similar to russian мамина комната), NOUN or ADJ?
+    '+pos<adj><ma>': ('Poss=Yes', 'PossGender=Masc'),
+    '+pos<adj><f>': ('Poss=Yes', 'PossGender=Fem'),
+    '+pos<adj><nt>': ('Poss=Yes', 'PossGender=Masc'),
+    '<nm>': 'Gender=Fem,Neut', # not masculine in Polish
+    '<dim>': 'Degree = Dim', #Polish, to check further grammatically
+    '<ela>': '', # я пока не поняла, что это, но этот тег есть у ADJ (<adj><ela> - premalo, prehudo, prehitro ) , которые образованы от ADV, оттуда они тащат этот тег)  ADV(<adv><sint><ela> - malo, hudo, hitro ). У всех ADJ с этим тегом есть префикс -pre- . Сокращение похоже на элатив, (и некоторые разметки Апертиума говорят, что это сокращение для элатива, но я не уверена. Выглядит, что в словенском нет элатива, плюс, его нет у этих прилагательных.)
+    '<nonpast>': '', # ukrainian: occurs in seq <vblex><dual>(here either <tv> or <iv>)<nonpast>, но дальше не нашла ничего хорошего
     # bidirectional tags: fully compatible between UD and Apertium
     '<n>': 'NOUN',
     '<vblex>': 'VERB',
@@ -38,6 +64,7 @@ udtags = {
     # имперфекта, нашла несколько разметок апертиума, где этот тег был для имперфекта. Также нашла, что в Апертиуме
     # фича imperfect это ‘pii’ и замаппен через UD как  Tense=Past Mood=Ind. Также в UD есть разметка Aspect = Imp.
     # Что выбрать? Алсо, в хорватском imperfect расценивается как архаизм и стилистический маркер (Nastya)
+    # SLOVENIAN: mostly occurs in a sequence <vblex><imperf><supn> = это несовершенный вид (широко распространен в отличие от хорватского)
     '<dual>': 'Aspect=Imp,Perf',
     '<pres>': 'Tense=Pres',
     '<past>': 'Tense=Past',
@@ -71,28 +98,13 @@ udtags = {
     '<pii>': 'Tense=Imp', # Bulgarian and Croatian, following UD tagset as per their website
     '<ssup>': 'Degree=Abs',
     '<futI>': 'Tense=Fut', #serbo-croatian, 2 futures (I and II), UD does not distinguish
-    #'<adv>+': 'Degree=Sup'
-
-    # unidirectional tags (not present in UD or used differently)
-    '<iv>': 'Valency=1',
-    '<tv>': 'Valency=2',
-    '<ref>': 'Valency=Refl',  # which tagset to follow for this?
-    '<emph>': 'VerbType=Emph',  # check consistency with other tagsets
-    '<fac>': 'VerbType=Fact',  # don't add this one наверн, эта разметка на типы глагола есть только в Апертиуме
-    '<itg>': 'Type=Inter',  # Possible for verbs as well as for adj and adv, check how to tag properly
-    '<cmp>': 'Degree=Com',
-    '<prx>': 'Definite=Prx',  # Proximal defined article, Macedonian (no UD model)
-    '<dst>': 'Definite=Dst',  # Distal defined article, Macedonian (no UD model)
-    '<adj><po>': 'ADV',  # Czech, chorvatsky = по-хорватски, it is an "adverbiated adjective"
-    '+htjeti': '', # Croatian, всегда ходят такой цепочкой: <inf>+htjeti<vbmod>. Excluding modal as it concurs to form a regular future form. встречается как кластер <inf>+htjeti<vbmod><clt><futI>. Здесь конструкция хотеть+глагол, где клитик отвечает за окончание глагола “хотеть” (htjeti)
-    '<vbmod>': '', # Excluding modal as it is a closed class, only in readings like htjeti or moguci (serbian), where it is an adverbial derived form
-    '<clt>': '', # clitics, same reason as per '+htjeti', could be used to mark the "reduced" form of FutI in serbo-croatian if we want to mark it (brace vs. brat ce (more common))
-    '<nt>+pos<adj>': 'Poss=Yes' # same reason as htjeti, used to build possessive constructions for name (similar to russian мамина комната)
-
+    '<cni>': 'Mood=Cnd',
+    '<mp>': 'Gender=Masc', #masculine personal in Polish
+    '<supn>': 'VerbForm=Sup',
 }
 
 
-with open(r'E:\Lorenzo Tosi\OneDrive - НИУ Высшая школа экономики\Documenti\Program\NIS-FREQ\frequency\hrmorph.txt-yes-top', 'r', encoding='utf8') as f:
+with open(r'D:\loren\OneDrive - НИУ Высшая школа экономики\Documenti\Program\NIS-FREQ\frequency\hrmorph.txt-yes-top', 'r', encoding='utf8') as f:
     analyses = []
     for line in f:
         analyses.append(line.split()[1].strip('^$'))
@@ -101,9 +113,10 @@ with open(r'E:\Lorenzo Tosi\OneDrive - НИУ Высшая школа эконо
         for aptag in udtags.keys():
             if aptag == '<n>' or aptag == '<vblex>' or aptag == '<adj>' or aptag == '<adj><po>':
                 analysis = analysis.replace(aptag, '\t' + udtags[aptag])
-            elif aptag == '<ma>' or aptag == '<mi>' or aptag == '<pp>' or aptag == '<pprs>'\
-                    or aptag == '<pp><adv>' or aptag == '<pprs><adv>':
-                analysis = analysis.replace(aptag, '|' + udtags[aptag][0] + '|' + udtags[aptag][1] + '|')
+            elif aptag == '<ma>' or aptag == '<mi>' or aptag == '<pp>' or aptag == '<pprs>' or aptag == '<pp><adv>' \
+                or aptag == '<pprs><adv>' or aptag == '+pos<adj><mi>' or aptag == '+pos<adj><ma>' \
+                    or aptag == '+pos<adj><f>' or aptag == '+pos<adj><nt>':
+                analysis = analysis.replace(aptag, '|' + udtags[aptag][0] + '|' + udtags[aptag][1])
             elif aptag == "<adv>" and "+" in analysis:
                 analysis = analysis.replace(aptag, "")
             else:
@@ -126,11 +139,5 @@ with open(r'E:\Lorenzo Tosi\OneDrive - НИУ Высшая школа эконо
             gloss = '|'.join(sorted(gloss.split('|')[1:])).strip('|')
             # discard duplicated verb forms
             gloss = gloss.replace('VerbForm=Part|VerbForm=Vadv', '|' + 'VerbForm=Vadv')
-            # if '<' in gloss:
-            print(surface_form, lemma_and_pos, gloss, sep='\t', file=open(r'E:\Lorenzo Tosi\OneDrive - НИУ Высшая школа экономики\Documenti\Program\NIS-FREQ\frequency\testfile.txt', 'a+', encoding='utf8'))
-
-               
-# for hr: (croatian)
-    # lp = ??
-# also .conllu files (in chat)    
-    
+            if '<' in gloss:
+                print(surface_form, lemma_and_pos, gloss, sep='\t', file=open(r'D:\loren\OneDrive - НИУ Высшая школа экономики\Documenti\Program\NIS-FREQ\frequency\testfile.txt', 'a+', encoding='utf8'))
